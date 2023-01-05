5471f13570c0b9e28a00ede2a9bcf7ec037d9696
Arnold6666 <a0919691671@gmail.com>
2023-01-05 16:29:31 +0800

    20230105_bs5_end

--- HTML_CSS_jQ_js/Ch18/checkbox.js
+++ HTML_CSS_jQ_js/Ch18/checkbox.js
@@ -5,3 +5,28 @@ Vue.createApp({
     }
   }
 }).mount('#app');
+
+Vue.createApp({
+  data(){
+    return{
+      message:''
+    }
+  }
+}).mount('#textarea');
+
+Vue.createApp({
+  data(){
+    return{
+      education:''
+    }
+  }
+}).mount('#dropdown');
+
+Vue.createApp({
+  data(){
+    return{
+      num1: 0,
+      num2: 0
+    }
+  }
+}).mount('#sum');
