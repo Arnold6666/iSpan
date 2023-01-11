a6bfc9059da0fe43071cd704d608ae48713cf0ff
Arnold6666 <a0919691671@gmail.com>
2023-01-11 16:26:31 +0800

    20230111_JS_day4

---
+++ JavaScript/20230111/data.js
@@ -0,0 +1,20 @@
+let jsonString = `[
+  {
+    "title": "珍珠奶茶",
+    "type": "hot iced",
+    "price": 60,
+    "popularity": true
+  },
+  {
+    "title": "茉香綠茶",
+    "type": "iced",
+    "price": 55,
+    "popularity": false
+  },
+  {
+    "title": "芋頭珍奶",
+    "type": "iced",
+    "price": 80,
+    "popularity": true
+  }
+]`
